582caf5fe88288e8313b9c4feba14ddf122bb96e
JMSBRN <zakhavai@gmail.com>
2022-08-18 13:54:28 +0300

    fix: update Wether values

--- src/features/api-weather/Weather.tsx
+++ src/features/api-weather/Weather.tsx
@@ -8,17 +8,11 @@ const Weather = () => {
   const {
     city,
     temp,
-    feelsLike,
-    tempMin,
-    tempMax,
     pressure,
     humidity,
-    seaLevel,
-    grndLevel,
     description,
     main,
     windDeg,
-    windGust,
     windSpeed,
   } = weatherValues;
 
@@ -26,19 +20,13 @@ const Weather = () => {
     <Styled.WeatherWrapper>
       <div> city: {city}</div>
       <div> temp: {temp}</div>
-      <div> feelsLike: {feelsLike}</div>
-      <div> tempMin: {tempMin}</div>
-      <div> tempMax : {tempMax}</div>
-      <div> pressure : {pressure}</div>
-      <div> humidity : {humidity}</div>
-      <div> seaLevel: {seaLevel}</div>
-      <div> grndLevel :{grndLevel}</div>
+      <div> pressure(mmHg) : {pressure}</div>
+      <div> humidity : {humidity}%</div>
       <div> description :{description}</div>
       <div> main :{main}</div>
       <ul>
-        <li>wind-degr : {windDeg}</li>
-        <li>wind gust :{windGust}</li>
-        <li>wind speed :{windSpeed}</li>
+        <li>wind-direction : {windDeg}</li>
+        <li>wind speed (km/h) :{windSpeed}</li>
       </ul>
     </Styled.WeatherWrapper>
   );
--- src/features/api-weather/apiWeatherSlice.ts
+++ src/features/api-weather/apiWeatherSlice.ts
@@ -4,18 +4,12 @@ import { RootState } from "../../app/store";
 export interface IWeatherValues {
   city: string;
   temp: number;
-  feelsLike: number;
-  tempMin: number;
-  tempMax: number;
   pressure: number;
   humidity: number;
-  seaLevel: number;
-  grndLevel: number;
   description: string;
   main: string;
   windDeg: string;
-  windGust: string;
-  windSpeed: string;
+  windSpeed: number;
 }
 const initialState = {
   apiData: "",
@@ -27,50 +21,62 @@ function temperatureConverter(valNum: string) {
   val = Math.round(val - 273.15);
   return val;
 }
+function pressureConverter(valNum: number) {
+  let val = valNum;
+  val = Math.round((val * 750.1) / 1000);
+  return val;
+}
 
 export const getAsyncApiWeather = createAsyncThunk(
   "weather",
   async (_, { dispatch }) => {
-    const CITY_NAME = "Minsk";
+    const CITY_NAME = "Borisov";
     const API_KEY = "516fa3e2ca0738cc84373fe362d7f8b6";
     const response = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
     );
     const json = await response.json();
-    console.log(json);
     const weatherVal = Object.values(json.main);
     const city = json.name;
     const temp = temperatureConverter(weatherVal[0] as string);
-    const feelsLike = temperatureConverter(weatherVal[1] as string);
-    const tempMin = temperatureConverter(weatherVal[2] as string);
-    const tempMax = temperatureConverter(weatherVal[3] as string);
-    const pressure = Number(weatherVal[4]);
+    const pressure = pressureConverter(Number(weatherVal[4]));
     const humidity = Number(weatherVal[5]);
-    const seaLevel = Number(weatherVal[6]);
-    const grndLevel = Number(weatherVal[7]);
     const descriptionWeather = json.weather[0];
     const description = descriptionWeather.description;
     const main = descriptionWeather.main;
     const wind = json.wind;
     const windDeg = wind.deg;
-    const windGust = wind.gust;
-    const windSpeed= wind.speed;
+    const windSpeed = Math.round(parseInt(wind.speed) * 3.257918552036199);
+    const speedIndexDirection = Math.round((parseInt(windDeg) / 360) * 16);
+    const windDirections = [
+      "N",
+      "NNE",
+      "NE",
+      "NEE",
+      "E",
+      "EES",
+      "ES",
+      "ESS",
+      "S",
+      "SSW",
+      "SW",
+      "SWW",
+      "W",
+      "WWN",
+      "WN",
+      "WNN",
+    ];
+    const winDir = windDirections[speedIndexDirection];
+
     const weatherValues: IWeatherValues = {
       city: city,
       temp: temp,
-      feelsLike: feelsLike,
-      tempMin: tempMin,
-      tempMax: tempMax,
       pressure: pressure,
       humidity: humidity,
-      seaLevel: seaLevel,
-      grndLevel: grndLevel,
       description: description,
       main: main,
-      windDeg: windDeg,
-      windGust: windGust,
+      windDeg: winDir,
       windSpeed: windSpeed,
-      
     };
     dispatch(setWeherValues(weatherValues));
   }
@@ -89,8 +95,7 @@ export const apiWeatherSlice = createSlice({
   },
   extraReducers(builder) {
     builder.addCase(getAsyncApiWeather.pending, () => {});
-    builder.addCase(getAsyncApiWeather.fulfilled, () => {
-    });
+    builder.addCase(getAsyncApiWeather.fulfilled, () => {});
   },
 });
 export const { getApiData, setWeherValues } = apiWeatherSlice.actions;
