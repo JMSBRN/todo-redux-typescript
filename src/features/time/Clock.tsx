1ac7a13470818df2f5096ca493a66bd808ea7a91
JMSBRN <zakhavai@gmail.com>
2022-08-31 12:33:42 +0300

    feat: created greeting name functional

--- src/features/time/Clock.tsx
+++ src/features/time/Clock.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { greetingsChange, selectTime, timeChange } from "./TimeSlice";
+import {
+  greetingsChange,
+  selectTime,
+  setIsName,
+  setName,
+  timeChange,
+} from "./TimeSlice";
 
 const Clock = () => {
-  const { time, date, greeting } = useAppSelector(selectTime);
+  const { time, date, greeting, isName, name } = useAppSelector(selectTime);
   const dispatch = useAppDispatch();
   useEffect(() => {
     setInterval(() => {
@@ -11,22 +17,38 @@ const Clock = () => {
     }, 1000);
   }, [dispatch]);
   useEffect(() => {
-    const curHrs = parseInt(JSON.stringify(new Date().getHours() / 24 * 4));
+    const curHrs = parseInt(JSON.stringify((new Date().getHours() / 24) * 4));
     setInterval(() => {
-      const  greet = [
-          'What are you doing that early?',
-          'Good Morning',
-          'Good Afternoon',
-          'Good Evening'
+      const greet = [
+        "What are you doing that early?",
+        "Good Morning",
+        "Good Afternoon",
+        "Good Evening",
       ][curHrs];
       dispatch(greetingsChange(greet));
-      localStorage.setItem('greeting', JSON.stringify(greet))
-  }, 1000)  
-  },[dispatch, greeting])
-  const greetingFromLocal = localStorage.getItem('greeting');
+      localStorage.setItem("greeting", JSON.stringify(greet));
+    }, 1000);
+  }, [dispatch, greeting]);
+  const greetingFromLocal = localStorage.getItem("greeting");
+  const nameFromLocal = JSON.parse(localStorage.getItem("name") || '""');
+  if (nameFromLocal === "") {
+    dispatch(setIsName(false));
+  }
   return (
     <>
-    <div>{ greetingFromLocal }</div>
+      <div>{greetingFromLocal}</div>
+      {isName ? (
+        <div>{nameFromLocal}</div>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="please enter your name"
+            onChange={(e) => dispatch(setName(e.target.value))}
+          />
+          <button onClick={() => dispatch(setIsName(true))}>enter</button>
+        </>
+      )}
       <div>{time}</div>
       <div className="">{date}</div>
     </>
--- src/features/time/TimeSlice.ts
+++ src/features/time/TimeSlice.ts
@@ -1,36 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
- export interface IOption {
-    weekday: "long" | "short" | "narrow" | undefined;
-    year: "numeric" | "2-digit" | undefined;
-    month: "long" | "short" | "narrow" | "numeric" | "2-digit" | undefined;
-    day: "numeric" | "2-digit" | undefined;
+export interface IOption {
+  weekday: "long" | "short" | "narrow" | undefined;
+  year: "numeric" | "2-digit" | undefined;
+  month: "long" | "short" | "narrow" | "numeric" | "2-digit" | undefined;
+  day: "numeric" | "2-digit" | undefined;
 }
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const date = new Date().toLocaleDateString('en-US', options as IOption);
-
-const initialState = {
-    time: new Date().toTimeString().split(" ")[0],
-    date: date,
-    greeting: '',                                                                                             
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+const date = new Date().toLocaleDateString("en-US", options as IOption);
+interface IState {
+  time: string;
+  date: string;
+  greeting: string;
+  name: string;
+  isName: boolean;
 }
+const initialState: IState = {
+  time: new Date().toTimeString().split(" ")[0],
+  date: date,
+  greeting: "",
+  name: "",
+  isName: true,
+};
 export const timeSlice = createSlice({
-    name: 'time',
-    initialState,
-    reducers: {
-        timeChange: (state, action) => {
-            state.time = action.payload;
-        },
-        dateChange: (state, action) => {
-            state.date = action.payload
-        },
-        greetingsChange: (state, action) => {
-          state.greeting = action.payload;
-        }
-    }
+  name: "time",
+  initialState,
+  reducers: {
+    timeChange: (state, action) => {
+      state.time = action.payload;
+    },
+    dateChange: (state, action) => {
+      state.date = action.payload;
+    },
+    greetingsChange: (state, action) => {
+      state.greeting = action.payload;
+    },
+    setName: (state, action) => {
+      state.name = action.payload;
+      localStorage.setItem("name", JSON.stringify(state.name));
+    },
+    setIsName: (state, action) => {
+      state.isName = action.payload;
+    },
+  },
 });
 
-export const { timeChange, dateChange, greetingsChange } = timeSlice.actions;
+export const { timeChange, dateChange, greetingsChange, setName, setIsName } =
+  timeSlice.actions;
 export const selectTime = (state: RootState) => state.time;
 export default timeSlice.reducer;
