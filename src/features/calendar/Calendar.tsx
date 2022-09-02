3e8b7b505285845119a578b251e267428b62439d
JMSBRN <zakhavai@gmail.com>
2022-09-02 11:56:54 +0300

    fix: updated style for  Calendar  comp.

--- src/features/calendar/Calendar.tsx
+++ src/features/calendar/Calendar.tsx
@@ -7,7 +7,13 @@ const Calendar = () => {
   return (
     <div>
       <Styled.Calendar>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        <FullCalendar 
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekends={true}
+        firstDay={1}
+        contentHeight={300}
+        />
       </Styled.Calendar>
     </div>
   );
