0e0e48b848796ca683e896c70aac324d595e1cfc
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-09-15 21:26:51 +0300

    feat:  create translation functional for geeting message.

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './main.css';
+import './i118n';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
