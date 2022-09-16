ff312af7473567101bbc1f9c43a750f97b45b83c
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-09-16 21:22:43 +0300

    feat: create translate functional for Calendar comp.

--- src/features/calendar/Calendar.tsx
+++ src/features/calendar/Calendar.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import * as Styled from "./Calendar.style";
+import ruLocale from '@fullcalendar/core/locales/ru';
+import enLocale from '@fullcalendar/core/locales/en-gb';
 
 const Calendar = () => {
+  const languageFromLocal = (JSON.parse(localStorage.getItem('language')|| '"en"'));
   return (
     <div>
       <Styled.Calendar>
@@ -12,6 +15,7 @@ const Calendar = () => {
         initialView="dayGridMonth"
         weekends={true}
         firstDay={1}
+        locale={languageFromLocal ==='ru' ? ruLocale : enLocale}
         />
       </Styled.Calendar>
     </div>
