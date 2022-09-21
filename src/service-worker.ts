df3bc08a2713eaabc997a4ae96910a76c1336502
JMSBRN <zakhavai@gmail.com>
2022-09-21 16:48:04 +0300

    fix: createed locales folder to precache functional in workbox

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -57,7 +57,7 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),  
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
@@ -66,6 +66,12 @@ registerRoute(
       // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
     ],
+  })
+);
+registerRoute(
+  /locales/,  
+  new StaleWhileRevalidate({
+    cacheName: 'locales'
   })
 );
 
